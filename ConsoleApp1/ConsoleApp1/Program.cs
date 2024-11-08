210639c19a4517bdaeb2d1c260291b5413dba742
Leonardo <lugresic@etfos.hr>
2024-11-08 15:57:00 +0100

    cherry pick

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Leonardo ugrešić, broj indexa: 21321321");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od[Ime Prezime]");
+>>>>>>> cabd25d (Aplikacija salje pozdrav)
         }
     }
 }
